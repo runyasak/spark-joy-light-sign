161df1c0247b169617c8d2fb39e837ad18493d4d
Runyasak Chaengnaimuang <world@odds.team>
2023-02-18 23:58:27 +0700

    Add display logic for 'Show your sign' button in App component.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ import Splitting from 'splitting';
 const App: Component = () => {
   const [signText, setSignText] = createSignal('Spark Joy Light Sign');
 
+  const handleShowSign = () => {
+    const elem = document.documentElement;
+    elem.requestFullscreen();
+  };
+
   return (
     <div class="flex flex-col h-screen items-center justify-center bg-[#282c34] px-12 gap-8">
       <div
@@ -15,7 +20,7 @@ const App: Component = () => {
           by: 'chars',
         })}
       ></div>
-      <div class="absolute bottom-24 flex flex-col gap-4">
+      <div class="actions-sector">
         <input
           type="text"
           class="sign-text-input !bg-opacity-50"
@@ -23,6 +28,13 @@ const App: Component = () => {
           value={signText()}
           onInput={(e) => setSignText(e.currentTarget.value)}
         />
+
+        <button
+          class="text-white bg-gradient-to-r from-sky-400 via-sky-500 to-sky-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-sky-300 dark:focus:ring-sky-800 font-medium rounded-lg text-xl px-5 py-2.5 text-center mr-2 mb-2"
+          onClick={handleShowSign}
+        >
+          Show your sign
+        </button>
       </div>
     </div>
   );
