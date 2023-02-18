4b86aa8c2f6703d359fb69995f9fa5191b1a4e1d
Runyasak Chaengnaimuang <world@odds.team>
2023-02-18 23:40:23 +0700

    Add input field for editing sign text in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import type { Component } from 'solid-js';
+import { Component, createSignal } from 'solid-js';
 
 import Splitting from 'splitting';
 
 const App: Component = () => {
+  const [signText, setSignText] = createSignal('Spark Joy Light Sign');
+
   return (
-    <div class="flex flex-col h-screen items-center justify-center bg-[#282c34] px-12">
+    <div class="flex flex-col h-screen items-center justify-center bg-[#282c34] px-12 gap-8">
       <div
         class="rainbow-text animated"
         style={{ 'text-align': 'center' }}
         innerHTML={Splitting.html({
-          content: 'Spark Joy Light Sign',
+          content: signText(),
           by: 'chars',
         })}
       ></div>
+      <div class="absolute bottom-24 flex flex-col gap-4">
+        <input
+          type="text"
+          class="sign-text-input"
+          placeholder="Type your preview sign here..."
+          value={signText()}
+          onInput={(e) => setSignText(e.currentTarget.value)}
+        />
+      </div>
     </div>
   );
 };
