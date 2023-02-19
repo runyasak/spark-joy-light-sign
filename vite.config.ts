50360a8c6b962fcb56dc80274d38b1809ff1cf84
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2023-02-19 09:20:38 +0700

    Config github actions

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  base: "/spark-joy-light-sign/",
   plugins: [solidPlugin()],
   server: {
     port: 3000,
